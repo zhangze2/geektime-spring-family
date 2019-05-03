d17c7ba6e38cdd78e7de107d55bd674c0d23ff76
zhangze <zhangze.zz@qq.com>
2019-05-03 16:57:12 +0800

    拦截注释

--- Chapter 6/context-hierarchy-demo/src/main/java/geektime/spring/web/foo/FooAspect.java
+++ Chapter 6/context-hierarchy-demo/src/main/java/geektime/spring/web/foo/FooAspect.java
@@ -3,7 +3,7 @@ package geektime.spring.web.foo;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
-
+//拦截所有 testBean*
 @Aspect
 @Slf4j
 public class FooAspect {
